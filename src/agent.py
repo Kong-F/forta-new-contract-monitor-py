d07a945d6aca5d83334c9dcbbce16d1ef9930cba
Kong-F <fcs258852@gmail.com>
2022-07-22 14:26:23 +0800

    Uptated bot

--- src/agent.py
+++ src/agent.py
@@ -1,29 +1,23 @@
 import rlp
 import forta_agent
-from sqlalchemy import null
 from web3 import Web3
 from forta_agent import Finding, FindingType, FindingSeverity
-
-def calc_contract_address(address, nonce) -> str:
-    address_bytes = bytes.fromhex(address[2:].lower())
-    return Web3.toChecksumAddress(Web3.keccak(rlp.encode([address_bytes, nonce]))[-20:])
 
 
 def detect_contract_creations(transaction_event: forta_agent.transaction_event.TransactionEvent) -> list:
     findings = []
     
     tx = transaction_event.transaction
-    if (tx.to is null and len(tx.data) > 0):
-        created_contract_address = calc_contract_address(tx.from_, tx.nonce)
+    if (len(transaction_event.contract_address) > 0):
         findings.append(Finding({
             'name': 'New Contract',
-            'description': f'{tx.from_} created contract {created_contract_address}',
+            'description': f'{tx.from_} created contract {transaction_event.contract_address}',
             'alert_id': 'NEW-CREATION-MONITOR',
             'type': FindingType.Info,
             'severity': FindingSeverity.Info,
             'metadata': {
                 'creator': tx.from_,
-                'contract': created_contract_address,
+                'contract': transaction_event.contract_address,
                 'network': transaction_event.network,
                 'tx_hash': tx.hash
             }
--- src/agent_test.py
+++ src/agent_test.py
@@ -1,22 +1,17 @@
-from sqlalchemy import null
 import agent
 from forta_agent import create_transaction_event, FindingSeverity
 
 
 class TestContractCreationAgent:
-    def test_calc_contract_address(self):
-        contract_address = agent.calc_contract_address("0x81245e489cf1e02e38a83c2b9618826747410c50", 12)
-        assert contract_address == "0x2320A28f52334d62622cc2EaFa15DE55F9987eD9", "should be the same contract address"
-
-    
     def test_new_contract(self):
 
         tx_event = create_transaction_event({
             'network': 56,
+            'contract_address': '0x2320A28f52334d62622cc2EaFa15DE55F9987eD9',
             'transaction': {
                 'hash': "0x3bf16e29aa9acc6bbdf5557ed1241b03989246ca0f4f652e9122655390b4caed",
                 'from': "0x81245e489cf1e02e38a83c2b9618826747410c50",
-                'to': null,
+                'to': "0x0000000000000000000000000000000000000000",
                 'nonce': 12,
                 'data': '0x123'
             },
